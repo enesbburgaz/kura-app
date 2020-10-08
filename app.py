475bbe3a96fd6cc12c6a9a01fa9711b993162f62
enesbburgaz <enesbburgaz@gmail.com>
2020-10-08 13:56:34 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+import random
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/Enes/Documents/Projects/kura-app/kura.db'
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/result", methods = ["POST"])
+def result():
+    varlik = request.form.get("varlik").split()
+    baslik = request.form.get("baslik")
+    asilSayi = int(request.form.get("asil"))
+    yedekSayi = int(request.form.get("yedek"))
+    tarih = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    asilList = []
+    yedekList = []
+    if (asilSayi+yedekSayi) <= len(varlik):
+        for i in range(asilSayi):
+            item = varlik[random.randint(0,len(varlik)-1)]
+            if item not in asilList:
+                asilList.append(item)
+                varlik.remove(item)
+        for i in range(yedekSayi):
+            item = varlik[random.randint(0,len(varlik)-1)]
+            if item not in yedekList:
+                yedekList.append(item)
+                varlik.remove(item)
+    data=[
+    {
+    'tarih' : tarih,
+    'baslik': baslik,
+    'asilList': asilList,
+    'yedekList': yedekList,
+    'asil': asilSayi,
+    'yedek': yedekSayi
+    }]
+    return render_template("result.html", data = data)
+
+class Kuralar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tarih = db.Column(db.String(80), unique=True, nullable=False)
+    baslik = db.Column(db.String(120), unique=True, nullable=False)
+    asil = db.Column(db.String(120), unique=True, nullable=False)
+    yedek = db.Column(db.String(120), unique=True, nullable=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+if __name__ == "__main__":
+    app.run(debug = True)
